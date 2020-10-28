52e8c27d0aa3813aad3cb0bbd90112a83bdf45d7
Aaron Pierson <30605667+AaronPierson@users.noreply.github.com>
2020-10-27 19:39:28 -0500

    Search

--- Gamer's_Hub/app/src/main/java/com/example/gamers_hub/GamingHubActivity.kt
+++ Gamer's_Hub/app/src/main/java/com/example/gamers_hub/GamingHubActivity.kt
@@ -2,10 +2,12 @@ package com.example.gamers_hub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_gaminghub.*
 
 class GamingHubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gaminghub)
+        setSupportActionBar(toolbar)
     }
 }
